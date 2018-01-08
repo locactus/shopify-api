cba9e266c7d160fdb6e6cf0b67fdffbd5bf70b2b
Andrew MacBean <andrew.macbean@sky.uk>
2018-01-08 16:57:24 +0000

    added test for shopify instance

---
+++ shopify-updater/src/test/java/com/macbean/tech/shopify/ShopifyInstanceTest.java
@@ -0,0 +1,27 @@
+package com.macbean.tech.shopify;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class ShopifyInstanceTest {
+
+    private final ShopifyInstance testInstance = new ShopifyInstance(
+            "test.shopify.com",
+            "username",
+            "password"
+    );
+
+    @Test
+    public void getPutVariantUrl() {
+        assertThat(testInstance.getPutVariantUrl(1967l),
+                is("https://username;password@test.shopify.com/admin/variants/1967.json"));
+    }
+
+    @Test
+    public void getGetProductsUrl() {
+        assertThat(testInstance.getGetProductsUrl(),
+                is("https://username;password@test.shopify.com/admin/products.json"));
+    }
+}
